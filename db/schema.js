5f5ec8c6c206ac5d9beb4f615bcebdba3a3a2f25
tomatohammado <tomatohammado@gmail.com>
2019-04-20 14:29:39 -0400

    Format `db/schema.js` via Prettier

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,4 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yum');
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/yum");
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on("error", console.error.bind(console, "connection error:"));
