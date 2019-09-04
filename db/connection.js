a18d167041e576386fad4dba05bcc057f6ae60a0
Hammad Malik <tomatohammado@gmail.com>
2019-09-04 15:45:33 -0400

    Add `useUrlParser`

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/yum");
+mongoose.connect("mongodb://localhost/yum", { useNewUrlParser: true });
 mongoose.Promise = Promise;
 module.exports = mongoose;
